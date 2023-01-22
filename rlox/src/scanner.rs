37ec276a0a31d5605397ad822b4357bc8372ae60
Benjamin Anderson <benjamanderson10@gmail.com>
2023-01-21 18:21:30 -0700

    switching to vscode

--- rlox/src/scanner.rs
+++ rlox/src/scanner.rs
@@ -1,6 +1,7 @@
-
-use crate::{token::{TokenType, Token}, error::Error};
-
+use crate::{
+    error::Error,
+    token::{Token, TokenType},
+};
 
 pub struct Scanner<'a> {
     pub source: &'a String,
@@ -9,50 +10,152 @@ pub struct Scanner<'a> {
 }
 
 impl<'a> Scanner<'a> {
-
     pub fn new(source: &'a String) -> Self {
-        Scanner { source, tokens: Vec::new(), errors: Vec::new() }
+        Scanner {
+            source,
+            tokens: Vec::new(),
+            errors: Vec::new(),
+        }
     }
-    
+
     pub fn scan(&mut self) {
         let mut idx: usize = 0;
-                    
-                    // Single
-                    '(' => { self.tokens.push(Token { tokentype: TokenType::LeftParen, idx, offset: 1}) },
-                    ')' => { self.tokens.push(Token { tokentype: TokenType::RightParen, idx, offset: 1}) },
-                    '[' => { self.tokens.push(Token { tokentype: TokenType::LeftBrace, idx, offset: 1}) },
-                    ']' => { self.tokens.push(Token { tokentype: TokenType::RightBrace, idx, offset: 1}) },
-                    '{' => { self.tokens.push(Token { tokentype: TokenType::LeftCurlyBrace, idx, offset: 1}) },
-                    '}' => { self.tokens.push(Token { tokentype: TokenType::RightCurlyBrace, idx, offset: 1}) },
-                    ',' => { self.tokens.push(Token { tokentype: TokenType::Comma, idx, offset: 1}) },
-                    '.' => { self.tokens.push(Token { tokentype: TokenType::Dot, idx, offset: 1}) },
-                    ';' => { self.tokens.push(Token { tokentype: TokenType::Semicolon, idx, offset: 1}) },
-                    '/' => { self.tokens.push(Token { tokentype: TokenType::Slash, idx, offset: 1}) },
-                    '*' => { self.tokens.push(Token { tokentype: TokenType::Star, idx, offset: 1}) },
+        let chars = self.source.chars().into_iter();
 
-                    // Double
-                    '+' => { self.tokens.push(Token { tokentype: TokenType::LeftParen, idx, offset: 1}) },
-                    '-' => { self.tokens.push(Token { tokentype: TokenType::LeftParen, idx, offset: 1}) },
-                    '-' => { self.tokens.push(Token { tokentype: TokenType::LeftParen, idx, offset: 1}) },
-                    '-' => { self.tokens.push(Token { tokentype: TokenType::LeftParen, idx, offset: 1}) },
-                    '-' => { self.tokens.push(Token { tokentype: TokenType::LeftParen, idx, offset: 1}) },
-                    '-' => { self.tokens.push(Token { tokentype: TokenType::LeftParen, idx, offset: 1}) },
-                    '-' => { self.tokens.push(Token { tokentype: TokenType::LeftParen, idx, offset: 1}) },
-                    '-' => { self.tokens.push(Token { tokentype: TokenType::LeftParen, idx, offset: 1}) },
-                    '-' => { self.tokens.push(Token { tokentype: TokenType::LeftParen, idx, offset: 1}) },
-                    '-' => { self.tokens.push(Token { tokentype: TokenType::LeftParen, idx, offset: 1}) },
+        while let Some(c) = chars.next() {
+            match c {
+                // Single
+                '(' => self.tokens.push(Token {
+                    tokentype: TokenType::LeftParen,
+                    idx,
+                    offset: 1,
+                }),
+                ')' => self.tokens.push(Token {
+                    tokentype: TokenType::RightParen,
+                    idx,
+                    offset: 1,
+                }),
+                '[' => self.tokens.push(Token {
+                    tokentype: TokenType::LeftBrace,
+                    idx,
+                    offset: 1,
+                }),
+                ']' => self.tokens.push(Token {
+                    tokentype: TokenType::RightBrace,
+                    idx,
+                    offset: 1,
+                }),
+                '{' => self.tokens.push(Token {
+                    tokentype: TokenType::LeftCurlyBrace,
+                    idx,
+                    offset: 1,
+                }),
+                '}' => self.tokens.push(Token {
+                    tokentype: TokenType::RightCurlyBrace,
+                    idx,
+                    offset: 1,
+                }),
+                ',' => self.tokens.push(Token {
+                    tokentype: TokenType::Comma,
+                    idx,
+                    offset: 1,
+                }),
+                '.' => self.tokens.push(Token {
+                    tokentype: TokenType::Dot,
+                    idx,
+                    offset: 1,
+                }),
+                ';' => self.tokens.push(Token {
+                    tokentype: TokenType::Semicolon,
+                    idx,
+                    offset: 1,
+                }),
+                '/' => self.tokens.push(Token {
+                    tokentype: TokenType::Slash,
+                    idx,
+                    offset: 1,
+                }),
+                '*' => self.tokens.push(Token {
+                    tokentype: TokenType::Star,
+                    idx,
+                    offset: 1,
+                }),
 
-                    // Long
-                    '"' => { self.tokens.push(Token { tokentype: TokenType::LeftParen, idx, offset: 1}) },
-                    'a'..='z' | 'A'..='Z' | '_' => { 
-                        while 
+                // Double
+                '+' => self.tokens.push(Token {
+                    tokentype: TokenType::LeftParen,
+                    idx,
+                    offset: 1,
+                }),
+                '-' => self.tokens.push(Token {
+                    tokentype: TokenType::LeftParen,
+                    idx,
+                    offset: 1,
+                }),
+                '-' => self.tokens.push(Token {
+                    tokentype: TokenType::LeftParen,
+                    idx,
+                    offset: 1,
+                }),
+                '-' => self.tokens.push(Token {
+                    tokentype: TokenType::LeftParen,
+                    idx,
+                    offset: 1,
+                }),
+                '-' => self.tokens.push(Token {
+                    tokentype: TokenType::LeftParen,
+                    idx,
+                    offset: 1,
+                }),
+                '-' => self.tokens.push(Token {
+                    tokentype: TokenType::LeftParen,
+                    idx,
+                    offset: 1,
+                }),
+                '-' => self.tokens.push(Token {
+                    tokentype: TokenType::LeftParen,
+                    idx,
+                    offset: 1,
+                }),
+                '-' => self.tokens.push(Token {
+                    tokentype: TokenType::LeftParen,
+                    idx,
+                    offset: 1,
+                }),
+                '-' => self.tokens.push(Token {
+                    tokentype: TokenType::LeftParen,
+                    idx,
+                    offset: 1,
+                }),
+                '-' => self.tokens.push(Token {
+                    tokentype: TokenType::LeftParen,
+                    idx,
+                    offset: 1,
+                }),
 
-                        self.tokens.push(Token { tokentype: TokenType::Identifier(()), idx, offset: 1}) },
-                    '-' => { self.tokens.push(Token { tokentype: TokenType::LeftParen, idx, offset: 1}) },
+                // Long
+                '"' => self.tokens.push(Token {
+                    tokentype: TokenType::LeftParen,
+                    idx,
+                    offset: 1,
+                }),
+                'a'..='z' | 'A'..='Z' | '_' => self.tokens.push(Token {
+                    tokentype: TokenType::Identifier(()),
+                    idx,
+                    offset: 1,
+                }),
+                '-' => self.tokens.push(Token {
+                    tokentype: TokenType::LeftParen,
+                    idx,
+                    offset: 1,
+                }),
 
-                    _ => { self.errors.push(Error { line, column, message: format!("Unexpected character '{}'", c)}) },
-                    idx += 1;
-                }
-
+                _ => self.errors.push(Error {
+                    line,
+                    column,
+                    message: format!("Unexpected character '{}'", c),
+                }),
+            }
+        }
     }
-
+}
